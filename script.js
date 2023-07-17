e969e3551678f3b5634a233a3348688c3add658a
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-17 22:09:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // your code here
-let name = document.getElementById("name").value;
-let year =  document.getElementById("year").value;
+
 const btn =  document.getElementById("button");
 let h3 =  document.getElementById("url");
-btn.addEventListener("submit", (e)=>{
-	h3.innerText = ` https://localhost:8080/`
-	
+btn.addEventListener("click", (e)=>{
+	e.preventDefault();
+	let name = document.getElementById("name").value;
+let year =  document.getElementById("year").value;
+	console.log(name, year);
+	h3.innerText = `https://localhost:8080/?name=${name}&year=${year}`;
 })
